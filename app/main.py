389d4729a18c3e1f4219d0b033547602a54bc3d1
qecstr <yessenov.zhassulan@gmail.com>
2024-02-28 18:39:13 +0600

    fixing CORS with delete

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=['GET','POST','DELETE'],
     allow_headers=["*"],
 )
 class Finances(BaseModel):
